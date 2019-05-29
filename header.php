a4831bca0aecde8506fdb0e9f4bb87e44735e821
Rishab <rishabagrawal@hotmail.com>
2019-05-29 16:46:24 -0700

    add styling in button, give container margin,maxwidth,padding,width, typography give color fontfmaily linehight

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
 	<?php wp_head(); ?>
 	</head>
