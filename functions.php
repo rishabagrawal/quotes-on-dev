74592962f5fc33dae6a50161a4b56463aca94a77
Rishab <rishabagrawal@hotmail.com>
2019-05-29 15:43:25 -0700

    create fonts folder & file, add logo image, background black

--- functions.php
+++ functions.php
@@ -58,6 +58,12 @@ add_filter( 'stylesheet_uri', 'qod_minified_css', 10, 2 );
 function qod_scripts() {
 	wp_enqueue_style( 'qod-style', get_stylesheet_uri() );
 
+	//TODO add Font Awesome wp_enqueue_style, this is used for quotes 
+	// try adding the quotes with CSS ::after and ::before  psuedo element 
+
+     // :after ::after
+	//TODO add your own script.js file wp_enqueue_script 
+
 	wp_enqueue_script( 'qod-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'qod-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
 }
--- header.php
+++ header.php
@@ -17,13 +17,19 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
+		<div id="page" class="hfeed site container">
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<div class="logo">
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<h1 class="header-image">
+						<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/qod-logo.svg" alt="logo">
+						</a>
+						</h1>
+					</div>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
